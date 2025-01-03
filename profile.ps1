a7299ed2c108bce77a837ccac692faef16528e0c
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2025-01-03 15:03:26 +0100

    format

--- profile.ps1
+++ profile.ps1
@@ -20,17 +20,17 @@ Function cg { pushd c:\git }
 function ce {
     $counter = 0
     [array] $foundExes =
-        ls *.exe -Recurse `
-        | ? { $_.fullname.contains("bin") } `
-        | sort lastwritetime -Descending `
-        | group DirectoryName `
-        | % {
-            [array] $filenames = $_.Group | % {,$_.Name}
-            $mostRecent = $_.Group | % {$_.LastWriteTime} | sort -Descending | select -first 1
-            @{path=$_.Name; files = $filenames; mostrecent = $mostRecent}
-         } `
-        | sort -Descending -Property mostrecent `
-        | % { $counter = $counter + 1; $_.counter = $counter; ,$_}
+    ls *.exe -Recurse `
+    | ? { $_.fullname.contains("bin") } `
+    | sort lastwritetime -Descending `
+    | group DirectoryName `
+    | % {
+        [array] $filenames = $_.Group | % { , $_.Name }
+        $mostRecent = $_.Group | % { $_.LastWriteTime } | sort -Descending | select -first 1
+        @{path = $_.Name; files = $filenames; mostrecent = $mostRecent }
+    } `
+    | sort -Descending -Property mostrecent `
+    | % { $counter = $counter + 1; $_.counter = $counter; , $_ }
     if ($foundExes.Length -eq 0) {
         Write-Host "none found"
     }
@@ -39,10 +39,11 @@ function ce {
         | select -First 1 `
         | % { $_.path } `
         | pushd
-    } else {
-        $foundExes | % { Write-Host "$($_.counter) $($_.path) $($_.files)"}
+    }
+    else {
+        $foundExes | % { Write-Host "$($_.counter) $($_.path) $($_.files)" }
         $choice = Read-Host
-        $gohere = $foundExes | ? {$_.counter -eq $choice} | % {$_.path}
+        $gohere = $foundExes | ? { $_.counter -eq $choice } | % { $_.path }
         pushd $gohere
     }
 }
@@ -50,9 +51,10 @@ function ce {
 function cd_up { set-location ".." }
 function bfgFun { java -jar C:\programs\bfg-repo-cleaner\bfg-1.14.0.jar $args }
 function vsCodeFun {
-    if($args.Length -eq 0) {
+    if ($args.Length -eq 0) {
         code .
-    } else {
+    }
+    else {
         code $args
     }
 }
@@ -63,7 +65,7 @@ set-alias -name c -value vsCodeFun
 set-alias -name cn -value code # vscode new
 
 function curves {
-    ls -File -Recurse -include @("*.fs","*.json", "*.csv", "*.config", "*.py") | sls -Pattern "\d{6,}"
+    ls -File -Recurse -include @("*.fs", "*.json", "*.csv", "*.config", "*.py") | sls -Pattern "\d{6,}"
 }
 
 function total($path) {
@@ -74,11 +76,12 @@ function total($path) {
 
 function go-crazy() {
     # regex from https://stackoverflow.com/questions/11040707/c-sharp-regex-for-guid
-    $found = powercfg /l | sls crazy | % { $_ -match '(?im)[{(]?[0-9A-F]{8}[-]?(?:[0-9A-F]{4}[-]?){3}[0-9A-F]{12}[)}]?'}
-    if($found) {
+    $found = powercfg /l | sls crazy | % { $_ -match '(?im)[{(]?[0-9A-F]{8}[-]?(?:[0-9A-F]{4}[-]?){3}[0-9A-F]{12}[)}]?' }
+    if ($found) {
         powercfg /SETACTIVE $matches[0]
         Write-Output "going crazy!!!!"
-    } else {
+    }
+    else {
         Write-Output "oh no - crazy profile not found!"
     }
 }
@@ -87,9 +90,9 @@ function start-fiddler() {
     # to make fiddler work with .js I needed to:
     # - trust root cert for HTTP
     # - enable TLS1.2 in protocols for HTTPS
-    $env:HTTP_PROXY ="http://127.0.0.1:8888"
-    $env:HTTPS_PROXY="http://127.0.0.1:8888"
-    $env:NODE_TLS_REJECT_UNAUTHORIZED=0
+    $env:HTTP_PROXY = "http://127.0.0.1:8888"
+    $env:HTTPS_PROXY = "http://127.0.0.1:8888"
+    $env:NODE_TLS_REJECT_UNAUTHORIZED = 0
     Start-Process "C:\programki\fifflerClassic\Fiddler.exe"
     Write-Output "tip - you can filter requests you see in fiddler by using Rules/User-Agents and set it to axios"
     Write-Output "you can also find the Windows proxy settings and disable the proxy that fidder set. This way you should only see request from your console."
@@ -106,7 +109,7 @@ function reset-fiddler() {
 function clean-git($masterBranch = "master") {
     git remote prune origin # remove remote branches that don't exist on origin
     $to_be_removed = git branch --merged $masterBranch --all | ? { ! $_.Contains($masterBranch) }
-    $local, $remote = $to_be_removed | Group-Object -Property {$_.Contains("remotes/origin")}
+    $local, $remote = $to_be_removed | Group-Object -Property { $_.Contains("remotes/origin") }
     foreach ($branch in $local.Group) {
         git branch -d $branch.Trim()
     }
