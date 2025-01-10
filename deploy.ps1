ba776690e74e70d3c21c7a230ee121e107692494
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2025-01-10 19:36:07 +0100

    create profile dir if it doesn't exist

--- deploy.ps1
+++ deploy.ps1
@@ -1,3 +1,8 @@
 # https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_profiles?view=powershell-7.4
-$profileLocation = "$HOME\Documents\PowerShell\Profile.ps1"
-Copy-Item -Path "./profile.ps1" -Destination $profileLocation
+$profileDir  = "$HOME\Documents\PowerShell"
+$profileFile = "$profileDir\Profile.ps1"
+if (-not (Test-Path $profileDir)) {
+    # The "PowerShell" might not exist, but it's the only directory in the path that might be missing
+    New-Item -ItemType Directory -Path $profileDir
+}
+Copy-Item -Path "./profile.ps1" -Destination $profileFile
