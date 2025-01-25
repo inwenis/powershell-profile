da70f545365393caea62a7e4e1d52c5561026afa
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2025-01-26 00:41:09 +0100

    test 1 fails

--- Test.ps1
+++ Test.ps1
@@ -1,22 +1,34 @@
 Import-Module Pester
-Import-Module ./Profile.ps1
+Import-Module ./Profile.ps1 -Force
 
-Describe 'Get-Planet' {
-    It 'Given no parameters, it lists all 8 planets' {
+$rootLocation = Get-Location
+
+BeforeAll {
+    mkdir "executing-tests-here"
+    pushd "executing-tests-here"
+}
+
+AfterAll {
+    Set-Location $rootLocation
+    Remove-Item "executing-tests-here" -Recurse -Force
+}
+
+Describe 'Clear-Git-Branches' {
+    It 'Removes a local branch merged into master' {
         mkdir "test-git-repo"
         pushd "test-git-repo"
         git init
         git commit --allow-empty -m "dummy commit 1" # we need a commit so that master branch doesn't disappear after creating a new branch
-        git checkout -b dummy-branch
+        git checkout -b dummy-branch *> $null # https://stackoverflow.com/questions/57538714/suppress-output-from-git-command-in-powershell
         git commit --allow-empty -m "dummy commit 2"
-        git checkout master
+        git checkout master *> $null
         git merge dummy-branch --no-ff --no-edit # no edit is needed to avoid editor opening
 
         Clear-Git-Branches
 
-        $all = git branch --all
+        $all = git branch --all | ForEach-Object { $_.Trim() }
         $all | Should -Not -Contain "dummy-branch"
         popd
-        Remove-Item "test-git-repo" -Recurse
+        Remove-Item "test-git-repo" -Recurse -Force
     }
 }
