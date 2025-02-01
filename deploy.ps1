04c2888ddd71dd4718be462533a05375a9cf903e
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2025-02-01 10:32:45 +0100

    comment

--- deploy.ps1
+++ deploy.ps1
@@ -2,7 +2,7 @@
 $profileDir  = "$HOME\Documents\PowerShell"
 $profileFile = "$profileDir\Profile.ps1"
 if (-not (Test-Path $profileDir)) {
-    # The "PowerShell" might not exist, but it's the only directory in the path that might be missing
+    # The "PowerShell" directory might not exist, but it's the only directory in the path that might be missing
     New-Item -ItemType Directory -Path $profileDir
 }
 Copy-Item -Path "./profile.ps1" -Destination $profileFile
