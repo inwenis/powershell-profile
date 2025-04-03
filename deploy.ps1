ad890b778ee2c472f143d2ad3dff88ae1ff35fcb
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2025-04-03 23:07:51 +0200

    improve git diff usage

--- deploy.ps1
+++ deploy.ps1
@@ -1,3 +1,42 @@
+function Git-Diff {
+    param (
+        [string] $fileA,
+        [string] $fileB
+    )
+
+    # if the file doesn't exist we use NUL which is Window's equivalent of /dev/null
+    # NUL is a special file that discards all data written to it, and returns EOF on read
+    # https://ss64.com/nt/nul.html
+    $fileA = if (Test-Path $fileA) { $fileA } else { "NUL" }
+    $fileB = if (Test-Path $fileB) { $fileB } else { "NUL" }
+    # --no-pager - if the diff is large we don't want pagination
+    # --color - force color output even if the output is not a terminal
+    # --no-index - one of the files is not in a git repository
+    $diff = git --no-pager diff --color --no-index $fileA $fileB
+    return $diff
+}
+
+function Deploy-File {
+    param (
+        [Parameter(ValueFromPipeline)] [System.IO.FileInfo] $file,
+                                       [string]             $destinationDir
+    )
+
+    process {
+        $fileIncoming = $file.FullName
+        $fileDeployed = Join-Path $destinationDir $file.Name
+
+        $diff = Git-Diff $fileDeployed $fileIncoming
+        $diff | Write-Host
+        if ($null -eq $diff) {
+            Write-Host "No changes in $($file.Name) to deploy"
+        } else {
+            Copy-Item -Path $fileIncoming -Destination $fileDeployed
+            Write-Host "Done deploying $($file.Name)"
+        }
+    }
+}
+
 # https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_profiles?view=powershell-7.4
 $profileDir   = "$HOME\Documents\PowerShell"
 $profileFile  = "$profileDir\Profile.ps1"
@@ -20,31 +59,10 @@ if ($null -eq $diff) {
     Write-Host "No changes to deploy"
 }
 Copy-Item -Path "./Profile.ps1" -Destination $profileFile
-Write-Host "Done"
-
-# make deploy file function accept pipe
-function Deploy-File {
-    param (
-        [Parameter(ValueFromPipeline)] [System.IO.FileInfo] $file
-    )
-
-    process {
-        $fileIncoming = $file.FullName
-        $fileDeployed = Join-Path $resourcesDir $file.Name
-
-        $diff = git --no-pager diff $fileDeployed $fileIncoming
-        git --no-pager diff $fileDeployed $fileIncoming # do it twice to preserve colours
-        if ($null -eq $diff) {
-            Write-Host "No changes to deploy"
-        } else {
-            Copy-Item -Path $fileIncoming -Destination $fileDeployed
-        }
-        Write-Host "Done deploying $($file.Name)"
-    }
-}
+Write-Host "Done deploying Profile.ps1"
 
 Write-Host "Deploying resources..."
-Get-ChildItem -Path "./resources" -File | Deploy-File
+Get-ChildItem -Path "./resources" -File | Deploy-File -destinationDir $resourcesDir
 
 # todo - can I use Reload-Profile from Profile.ps1 to reload the profile?
 New-ModuleManifest .\temp.psd1  -NestedModules "./Profile.ps1"
