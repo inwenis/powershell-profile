7d8cfa08b10bbf5aad2c2a35d2cc4b3daa0b5c18
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2025-04-03 22:31:37 +0200

    format

--- deploy.ps1
+++ deploy.ps1
@@ -1,7 +1,8 @@
 # https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_profiles?view=powershell-7.4
-$profileDir  = "$HOME\Documents\PowerShell"
-$profileFile = "$profileDir\Profile.ps1"
+$profileDir   = "$HOME\Documents\PowerShell"
+$profileFile  = "$profileDir\Profile.ps1"
 $resourcesDir = "$profileDir\resources"
+
 if (-not (Test-Path $profileDir)) {
     # The "PowerShell" directory might not exist, but it's the only directory in the path that might be missing
     New-Item -ItemType Directory -Path $profileDir | Out-Null
