9f2488db57697201eae13a03501fa72434690138
Filip Kucharczyk <inwenis@gmail.com>
2026-03-20 08:38:56 +0000

    moved to private dotfiles repo

--- resources/playground.fsx
+++
@@ -1,7 +0,0 @@
-#load "c:/git/prelude/prelude.fsx"
-
-Prelude.WorkingDirectorySetter.SetToMe()
-
-open System
-
-// --- header --- //
--- setup.ps1
+++
@@ -1,3 +0,0 @@
-Install-Module -Name Pester -Force -SkipPublisherCheck # using Pester 5.4 as of writing this
-Install-Module Microsoft.PowerShell.ConsoleGuiTools
-Install-Module -Name "F7History"
